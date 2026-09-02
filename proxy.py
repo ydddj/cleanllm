71650e3bd8f0cae780574dbaccda43aa74276a39
ydddj <iydddj@gmail.com>
2026-09-02 11:44:44 +0800

    style: align endpoint copy control

--- proxy.py
+++ proxy.py
@@ -63,7 +63,7 @@ DEFAULT_SETTINGS = {
     "appearance_background": "",
 }
 
-app = FastAPI(title="CleanLLM", version="1.0.37")
+app = FastAPI(title="CleanLLM", version="1.0.38")
 app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 OLLAMA_TASKS: dict[str, dict[str, Any]] = {}
 OLLAMA_HANDLES: dict[str, asyncio.Task] = {}
