35bee9586b156f784f82d7d887eb529cb1049faa
ydddj <iydddj@gmail.com>
2026-09-02 16:23:43 +0800

    上游名称现在可以直接编辑，输入后选项卡标题立即同步。
    上游名称现在可以直接编辑，输入后选项卡标题立即同步。

--- proxy.py
+++ proxy.py
@@ -69,7 +69,7 @@ DEFAULT_SETTINGS = {
     "appearance_mask_opacity": 69,
 }
 
-app = FastAPI(title="CleanLLM", version="1.0.64")
+app = FastAPI(title="CleanLLM", version="1.0.65")
 app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 OLLAMA_TASKS: dict[str, dict[str, Any]] = {}
 OLLAMA_HANDLES: dict[str, asyncio.Task] = {}
